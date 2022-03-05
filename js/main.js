886f591c36280cd6564a4cebe8f90027ff22fc52
moseskarani <karanim594@gmail.com>
2022-03-05 17:01:46 +0300

    create function for getting gender,var for gender value

--- js/main.js
+++ js/main.js
@@ -3,4 +3,15 @@ function myName() {
   let month = Number(documrnt.getElementById("month").value);
   let day = Number(documrnt.getElementById("day").value);
   let genders = document.getElementById("gender");
+
+  // getting gender
+  function gettingGender() {
+    for (let gender of genders) {
+      if (gender.selected) {
+        return gender.value;
+      }
+    }
+  }
+
+  let valueOfGender = gettingGender();
 }
