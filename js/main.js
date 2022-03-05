5fbff497b99ca3d0630456629ee2c83b96a0f205
moseskarani <karanim594@gmail.com>
2022-03-05 16:52:14 +0300

    create function, variables for date and gender input

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function myName() {
+  let year = document.getElementById("year").value;
+  let month = Number(documrnt.getElementById("month").value);
+  let day = Number(documrnt.getElementById("day").value);
+  let genders = document.getElementById("gender");
+}
