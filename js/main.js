e0fbb2894496aa9b13592e4ca07abfa7db4536b6
moseskarani <karanim594@gmail.com>
2022-03-05 17:51:52 +0300

    fix:form optimization

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function myName() {
   let year = document.getElementById("year").value;
-  let month = Number(documrnt.getElementById("month").value);
-  let day = Number(documrnt.getElementById("day").value);
+  let month = Number(document.getElementById("month").value);
+  let day = Number(document.getElementById("day").value);
   let genders = document.getElementById("gender");
 
   // getting gender
@@ -14,4 +14,22 @@ function myName() {
   }
 
   let valueOfGender = gettingGender();
+  console.log(valueOfGender);
+
+  // functions for validation
+  function checkMonth() {
+    if (month < 1 || month > 12) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function checkDay() {
+    if (month === 2 && Number(year)%4 === 0) {
+      if (day > 28 || day < 1) {
+
+      }
+    }
+  }
 }
